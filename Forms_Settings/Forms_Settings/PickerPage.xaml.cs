02944fdb0712366c2a39068735186a4e67833928
Salman Naveed Khan <86252559+Esabzayi@users.noreply.github.com>
2022-01-28 21:45:30 +0500

    Add project files.

---
+++ Forms_Settings/Forms_Settings/PickerPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Forms_Settings
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickerPage : ContentPage
+    {
+        public class GetData
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public PickerPage()
+        {
+            InitializeComponent();
+            foreach (var item in DataCon())
+            {
+                CodeData.Items.Add(item.Name);
+            }
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var contactmethod =  ContactMethod.Items[ContactMethod.SelectedIndex];
+            DisplayAlert("Selection", contactmethod, "Cancel");
+        }
+
+        private IList<GetData> DataCon()
+        {
+            return new List<GetData> {
+
+                new GetData { Id = 1, Name = "SMS"},
+                new GetData { Id = 2, Name = "Data"},
+                new GetData { Id = 3, Name = "Whatsappp"},
+                new GetData { Id = 4, Name = "GB"}
+
+            };
+        }
+    }
+}
